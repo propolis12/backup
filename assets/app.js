8d7a57af2d706e988029b8ba2fb36e4be0f99088
propolis12 <jfujas@gmail.com>
2021-01-16 17:40:45 +0100

    require form

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,19 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+import $ from 'jquery';
+
+/*
+ajax ukazka
+var $container = $('.js-vote-arrows');
+$container.find('a').on('click', function(e) {
+    e.preventDefault();
+    var $link = $(e.currentTarget);
+    $.ajax({
+        url: '/comments/10/vote/'+$link.data('direction'),
+        method: 'POST'
+    }).then(function(data) {
+        $container.find('.js-vote-total').text(data.votes);
+    });
+});
+ */
